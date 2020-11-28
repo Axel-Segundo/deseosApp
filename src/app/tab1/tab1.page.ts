4909803d1a333c0ea68c27c7440aa0715b603935
Axel Segundo <axeljasegundo@gmail.com>
2020-11-28 11:33:07 -0600

    121-125

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 import { DeseosService } from '../services/deseos.service';
 
 @Component({
@@ -8,6 +10,49 @@ import { DeseosService } from '../services/deseos.service';
 })
 export class Tab1Page {
 
-  constructor(public deseosService: DeseosService) {}
+  constructor(public deseosService: DeseosService,
+              private router: Router,
+              private alertCtrl: AlertController) {
+
+
+    }
+
+    async agregarLista(){
+      // this.router.navigateByUrl('/tabs/tab1/agregar');
+
+      const alert = await this.alertCtrl.create({
+         header: 'Nueva Lista',
+         inputs: [{
+           name: 'titulo',
+           type: 'text',
+           placeholder: 'Nombre de la lista'
+         }
+       ],
+         buttons: [
+           {
+           text: 'Cancelar',
+           role: 'cancel',
+           handler: () => {
+             console.log('Cancelar');
+              }
+           },
+           {
+             text: 'Crear',
+             handler: (data) => {
+               console.log(data);
+               if (data.titulo.length === 0) {
+                 return;
+               }
+
+               this.deseosService.crearLista(data.titulo);
+
+               // Tengo que crear la lista
+
+             }
+           }
+       ]
+      })
+      alert.present();
+}
 
 }
