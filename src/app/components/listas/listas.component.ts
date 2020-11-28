8d3a085b0564ce3dac84c80c49234855c153f978
Axel Segundo <axeljasegundo@gmail.com>
2020-11-28 12:45:49 -0600

    132-134

--- src/app/components/listas/listas.component.ts
+++ src/app/components/listas/listas.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController, IonList } from '@ionic/angular';
 import { Lista } from 'src/app/models/lista.model';
 import { DeseosService } from '../../services/deseos.service';
 
@@ -10,10 +11,13 @@ import { DeseosService } from '../../services/deseos.service';
 })
 export class ListasComponent implements OnInit {
 
+  @ViewChild(  IonList ) lista: IonList;
+
   @Input() terminada = true;
 
   constructor(public deseosService: DeseosService,
-              private router: Router) { }
+              private router: Router,
+              private alertCtrl: AlertController) { }
 
   ngOnInit() {}
 
@@ -25,6 +29,49 @@ export class ListasComponent implements OnInit {
       this.router.navigateByUrl(`/tabs/tab1/agregar/${lista.id}`);
     }
 
+
+  }
+  borrarLista(lista: Lista){
+    this.deseosService.borrarLista(lista);
+   }
+   async editarLista(lista: Lista){
+
+    const alert = await this.alertCtrl.create({
+      header: 'Editar Lista',
+      inputs: [{
+        name: 'titulo',
+        type: 'text',
+        value: lista.titulo,
+        placeholder: 'Nombre de la lista'
+      }
+    ],
+      buttons: [
+        {
+        text: 'Cancelar',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancelar');
+          this.lista.closeSlidingItems();
+           }
+        },
+        {
+          text: 'Actualizar',
+          handler: (data) => {
+            console.log(data);
+            if (data.titulo.length === 0) {
+              return;
+            }
+
+
+            lista.titulo = data.titulo;
+            this.deseosService.guardarStorage();
+            this.lista.closeSlidingItems();
+
+          }
+        }
+    ]
+   });
+    alert.present();
 
   }
 
